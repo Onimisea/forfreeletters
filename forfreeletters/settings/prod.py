730530a3a81accb0073b08c70f35d28d2bbc67d9
Onimisea <onimisea@gmail.com>
2024-03-11 21:07:07 +0100

    Done with productionn configurations for ForfreeLetters web app

---
+++ forfreeletters/settings/prod.py
@@ -0,0 +1,32 @@
+from .common import *
+
+# DEBUG = False
+
+DEBUG = os.getenv('DEBUG')
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(" ")
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
+    }
+}
+
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_ORIGINS').split(" ")
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
