a6bc00374cb0458e96e1aa9f1382639d4de5b5a9
Onimisea <onimisea@gmail.com>
2024-03-19 01:42:27 +0100

    new

--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import GenericTemplateList, SubcategoriesList
-
-urlpatterns = [
-    path('generic-templates/', GenericTemplateList.as_view(), name='generic_template_list'),
-    path('get-subcategories/<str:category>/', SubcategoriesList.as_view(), name='get_subcategories'),
-]
--- main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import HomeView, TemplatesView
-
-urlpatterns = [
-    path("", HomeView.as_view(), name='home'),
-    path("templates/", TemplatesView.as_view(), name='templates'),
-]
