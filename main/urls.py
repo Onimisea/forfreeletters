ef318887b9d073c05eeb11829bca90aa47821b05
Adebayo Otomuola <114394143+OtomuolaAdebayo@users.noreply.github.com>
2024-03-22 20:24:05 -0700

    privacy page

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import HomeView, TemplatesView
+from .views import HomeView, TemplatesView, PrivacyPageView, ContactView
 
 urlpatterns = [
     path("", HomeView.as_view(), name='home'),
     path("templates/", TemplatesView.as_view(), name='templates'),
+    path("privacy/", PrivacyPageView.as_view(), name='privacy_page'),
+    path("contacts/", ContactView.as_view(), name='contacts'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from django.views.generic import ListView
@@ -72,7 +74,23 @@ class TemplatesView(ListView):
         sort_by = self.request.GET.get('sort_by', '-date_added')
         queryset = queryset.order_by(sort_by)
 
-        return queryset
+        return 
+
+class PrivacyPageView(ListView):
+    model = GenericTemplate
+    template_name = "privacy_page.html"
+    context_object_name = "privacy_page"
+
+    def get_queryset(self):
+        pass
+
+class ContactView(ListView):
+    model = GenericTemplate
+    template_name = "contact.html"
+    context_object_name = "contact"
+
+    def get_queryset(self):
+        pass
 
 
 
